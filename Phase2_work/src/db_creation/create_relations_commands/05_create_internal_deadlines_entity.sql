9e247029b405e909b86a057cd24ff4aebbf9f118
Mathieu Poulin <Mathieu.Poulin@maine.edu>
2025-11-04 19:46:13 -0500

    add documents and deadlines scripts

---
+++ Phase2_work/src/db_creation/create_relations_commands/05_create_internal_deadlines_entity.sql
@@ -0,0 +1,14 @@
+/*
+    Internal Deadlines Entity Creation
+    Version: 4 November 2025
+    Author: Mathieu Poulin
+    Description: An Internal Deadlines entity and its attributes
+*/
+
+CREATE TABLE InternalDeadlines (
+    internal_deadline_id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+
+    deadline_name VARCHAR(100) NOT NULL,
+    deadline_date DATETIME NOT NULL,
+    FOREIGN KEY (application_id) REFERENCES Applications(application_id) ON DELETE CASCADE
+);
