dcdf2ab4ac5cf1b0741fba4b4284a195760cd5ee
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2025-11-08 18:37:44 -0500

    fixed research field creation logic

--- Phase2_work/src/entity_crud_operations/research_fields/create_research_fields.sql
+++ Phase2_work/src/entity_crud_operations/research_fields/create_research_fields.sql
@@ -1,38 +1,25 @@
 /*
-  update_users_research_field.sql
+  create_research_field.sql
   Author: Colby Wirth
   Version: 8 November 2025
-  Description: Add a new research field for a user, creating it if needed.
-               Limits each user to at most 10 research fields.
+  Description: Insert a new research field and associate it with a user.
 
   Parameters:
     - user_id: The user to associate with
     - research_field: Name of the research field (required, unique)
   
-  Returns: research_field_id of the newly created/existing field
+  Returns: research_field_id of the newly created or existing field
 */
 
--- Check current count
-SET @current_count = (
-    SELECT COUNT(*)
-    FROM UserResearchFields
-    WHERE user_id = %(user_id)s
-);
+-- Insert the research field if it doesn't already exist
+INSERT INTO ResearchField (research_field)
+VALUES (%(research_field)s)
+ON DUPLICATE KEY UPDATE research_field_id = LAST_INSERT_ID(research_field_id);
 
--- If user has fewer than 10 fields, insert
-IF @current_count < 10 THEN
+-- Map user to research field
+INSERT INTO UserResearchFields (user_id, research_field_id)
+VALUES (%(user_id)s, LAST_INSERT_ID())
+ON DUPLICATE KEY UPDATE user_id = user_id;
 
-    -- Insert research field if it doesn't exist
-    INSERT INTO ResearchField (research_field)
-    VALUES (%(research_field)s)
-    ON DUPLICATE KEY UPDATE research_field_id = LAST_INSERT_ID(research_field_id);
-
-    -- Map user to research field
-    INSERT INTO UserResearchFields (user_id, research_field_id)
-    VALUES (%(user_id)s, LAST_INSERT_ID())
-    ON DUPLICATE KEY UPDATE user_id = user_id;
-
-END IF;
-
--- Return ID of inserted/existing field
+-- Return the ID of the inserted or existing research field
 SELECT LAST_INSERT_ID() AS research_field_id;
