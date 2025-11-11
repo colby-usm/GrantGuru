3329f31e1fd4949013d4ec19033fa53df118f915
AbdullahiEDU <Abdullahi.Abdullahi@maine.edu>
2025-11-11 17:55:16 -0500

    Added the CRUD files for the application

---
+++ Phase2_work/src/db_crud/applications/delete_application.sql
@@ -0,0 +1,9 @@
+/*
+Application Deletion Script for Applications Table
+Version: 10 November 2025
+Author: Abdullahi Abdullahi
+Description:
+ Delete all applications in the table
+ No parameters required
+*/
+DELETE FROM Applications;
---
+++ Phase2_work/src/db_crud/applications/update_application_status.sql
@@ -0,0 +1,14 @@
+/*
+ Applications Update Procedure
+    Version: 10 November 2025
+    Author: Abdullahi Abdullahi
+    Description: Update an existing application's status based on application_id
+    Parameters:
+        - application_id: The unique identifier of the application to be updated (required)
+        - status: The new status for the application (required)
+*/
+UPDATE Applications
+SET 
+    status = TRIM(%(status)s)
+WHERE application_id = %(application_id)s;
+
