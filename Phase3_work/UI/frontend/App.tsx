52cd1df2ce128d93f5b9959b801c743970d9b01d
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2025-11-23 18:59:39 -0500

    updated howto

--- Phase3_work/UI/frontend/App.tsx
+++ Phase3_work/UI/frontend/App.tsx
@@ -1,10 +1,20 @@
+// App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./components/LandingPage";
+import { HomePage } from "./components/HomePage";
+import { UserPage } from "./components/UserPage";
 import { ThemeProvider } from "./components/ThemeProvider";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/user" element={<UserPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
