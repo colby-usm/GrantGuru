1ad82eccf5c1457bc948d1f51cd2b2028ed8ad65
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2025-11-29 21:12:01 -0500

    updating init files

--- Phase3_work/UI/api/__init__.py
+++ Phase3_work/UI/api/__init__.py
@@ -5,13 +5,11 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
 
-    #CORS(app, resources={r"/api/*": {"origins": "*"}})
     CORS(app)
 
     from api.auth import auth_bp
     from api.public import public_bp
 
-    # Register blueprints
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(public_bp, url_prefix="/api/public")
 
